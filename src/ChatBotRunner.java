3dddb0a2f38920027cb9f8a9e39a0273675102fe
123 <sample@bths.edu>
2022-09-28 12:26:33 -0400

    jokjoi

---
+++ src/ChatBotRunner.java
@@ -0,0 +1,9 @@
+public class ChatBotRunner {
+    public static void main (String[] args){
+        ChatBot debbie = new ChatBot("Debbie", 16);
+        debbie.greeting("Wafa");
+        debbie.weather();
+
+
+    }
+}
---
+++ src/CircleRunner.java
@@ -0,0 +1,19 @@
+public class CircleRunner {
+    public static void main(String[] args) {
+        Circle circle = new Circle(5.0);
+        //circle1.printArea();
+        circle.printArea();
+        //predict 25x3.14 will print.
+
+
+        // A. create a Circle object with radius 5.0
+
+        // B. locate the method defined in the Circle class and write
+        //    a line of code to call it on your object
+
+        // C. BEFORE running this code, PREDICT what gets printed
+
+        // D. run the code and confirm your prediction! Were you correct?
+    }
+}
+
